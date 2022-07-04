a32c65f99634199d0e6e887859ae04a892846aac
Vitaliy Olkhin <ovvitalik@gmail.com>
2022-07-04 15:34:25 +0500

    update main wiews

--- views/main.php
+++ views/main.php
@@ -13,7 +13,40 @@ use wco\kernel\WCO;
         <link rel="icon" href="<?=WCO::$config['protocol'].$_SERVER['SERVER_NAME']?>/default/favicon.ico" type="image/x-icon">
     </head>
     <body>
-        <?=$this->views?>
+        <div class="container">
+            <a href="<?=WCO::Url('/')?>">
+                <img src="/default/images/logotip.png" alt="alt" class="img-fluid"/>
+            </a>
+        </div>
+        
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xl-3 left_block">
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="<?=WCO::Url('/')?>">Главная</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="<?=WCO::Url('/wco/doc/')?>">Документация</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link" href="#">Ссылка</a>
+                                </li>
+                                <li class="nav-item">
+                                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Отключенная</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-xl-9 content">
+                    <?=$this->views?>
+                </div>
+            </div>
+        </div>
         <?=WCO::getBodyEnd()?>
     </body>
 </html>
