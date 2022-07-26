3d78cbf42281a1037ac88defcabc1cbc77899142
Vitaliy Olkhin <ovvitalik@gmail.com>
2022-07-26 19:09:51 +0500

    Испоавлена ошибка фильтрации, добавил коментарии к коду

--- kernel/Controller.php
+++ kernel/Controller.php
@@ -14,17 +14,30 @@ use wco\kernel\View;
 class Controller extends View{
     protected function FilteringFields($filter,string $allowed = null) {
         $filter = trim($filter);
-        $filter = strip_tags($filter, $allowed = null);
+        $filter = strip_tags($filter, $allowed);
         return $filter;
     }
     
+    /**
+     * Фильтр полученых данных от метода GET
+     * @param type $filter
+     * @param type $allowed
+     * @return type
+     */
     protected function FilterInputGet($filter, $allowed = null) {
         $filter = filter_input(INPUT_GET, $filter);
         $filter = $this->FilteringFields($filter, $allowed);
         return $filter;
     }
     
-    protected function FilterInputPost($filter,$allowed = null) {
+    /**
+     * Фильтр полученых данных от метода POST
+     * 
+     * @param type $filter
+     * @param string $allowed
+     * @return type
+     */
+    protected function FilterInputPost($filter,string $allowed = null) {
         $filter = filter_input(INPUT_POST, $filter);
         $filter = $this->FilteringFields($filter, $allowed);
         return $filter;
